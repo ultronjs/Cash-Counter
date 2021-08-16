df48de5b4b25d7a4713bb68e4209bc2e01d711e9
ultron07 <50156573+ultron07@users.noreply.github.com>
2021-08-17 01:10:03 +0530

    correction of the logical

--- app.js
+++ app.js
@@ -10,26 +10,24 @@ let noteCounter = Array(9).fill(0);
 
 
 btnProceed1.addEventListener("click",function(){
-  if(btnProceed2 == null && inputBillAmount.value > 0  ){
-  var input = document.createElement("input");
-  var button = document.createElement("button");
-  var div = document.getElementById("container-given-amount");
-  input.type = "number";
-  input.placeholder = "Enter the Given Amount";
-  input.id = "input-given-amount";
-  button.id = "btn-proceed-2";
-  button.innerText = "Proceed";
-  btnProceed1.remove();
-  div.appendChild(input);
-  div.appendChild(button);
-  inputGivenAmount = document.querySelector("#input-given-amount");
-  console.log(inputGivenAmount.value);
-  btnProceed2 = document.querySelector("#btn-proceed-2");
-  btnProceed2.addEventListener("click", countNotes)
-  }
-  else{
-    alert("Please Enter the Billed Amount")
-  }
+  if (inputBillAmount.value === ""){
+    alert("Please Enter the Billed Amount");
+  }else{
+    var input = document.createElement("input");
+    var button = document.createElement("button");
+    var div = document.getElementById("container-given-amount");
+    input.type = "number";
+    input.placeholder = "Enter the Given Amount";
+    input.id = "input-given-amount";
+    button.id = "btn-proceed-2";
+    button.innerText = "Proceed";
+    btnProceed1.remove();
+    div.appendChild(input);
+    div.appendChild(button);
+    inputGivenAmount = document.querySelector("#input-given-amount");
+    btnProceed2 = document.querySelector("#btn-proceed-2");
+    btnProceed2.addEventListener("click", countNotes);
+    } 
 })
 
 function createTable(){
@@ -52,36 +50,29 @@ function createTable(){
 
 }
 
-
-
 function countNotes(){
-  if(inputBillAmount.value > inputGivenAmount.value){
-    alert("The Amount Given is less than the Billed Amount")
-  }
-  else { 
-    if(inputGivenAmount.value >0){
-      console.log(btnProceed1);
-      console.log(inputGivenAmount);
-      let amount = inputGivenAmount.value - inputBillAmount.value;
+  if (inputBillAmount.value === "") {
+    alert("Please Enter the Billed Amount");
+  }else if (inputGivenAmount.value === "") {
+    alert("Please Enter the Given Amount");
+  } else if (Number(inputBillAmount.value) > Number(inputGivenAmount.value)) {
+    alert("The Amount Given is less than the Billed Amount");
+  } else {
+      console.log(typeof(inputBillAmount.value));
+      console.log(typeof (inputGivenAmount.value));
+      let amount = Number(inputGivenAmount.value) - Number(inputBillAmount.value);
       for (let i = 0; i < 9; i++) {
         if (amount >= notes[i]) {
           noteCounter[i] = Math.floor(amount / notes[i]);
           amount = amount - noteCounter[i] * notes[i];
         }
       }
-      for(i=0;i<noteCounter.length;i++){
-        sum += noteCounter[i]
+      for (i = 0; i < noteCounter.length; i++) {
+        sum += noteCounter[i];
       }
-        console.log(sum)
-        modal.style.display = "block";
-        createTable();
-        console.log(noteCounter)
-      }
-      else{
-        alert("Please Enter the Given Amount")
-      }
-  }
-    
+      modal.style.display = "block";
+      createTable();
+  } 
 }
 
 span.onclick = function () {
